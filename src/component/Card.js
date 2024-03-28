66bedb117b79e1941fb860af5fa5b228c74e2fbf
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2024-03-28 23:09:00 +0530

    Created responsive card

--- src/component/Card.js
+++ src/component/Card.js
@@ -3,17 +3,16 @@ import React from 'react'
 const Card = () => {
   return (
     <div className='flex h-screen justify-center items-center'>
-        <div className='flex flex-col gap-y-4 items-center p-10 w-1/4 border-2'>
-            <p>
+        <div className='flex flex-col gap-y-8 items-center p-5 md:p-10 w-3/4 lg:w-2/4 xl:w-1/4 border-2 rounded-xl'>
+            <p className='text-md md:text-xl lg:text-2xl font-bold'>
                 Card
             </p>
-            <p>
+            <p className='text-sm md:text-md lg:text-lg text-justify'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
-            <button>
+            <button className='text-sm md:text-md lg:text-lg border-2 rounded-lg p-3'>
                 Submit
             </button>
         </div>
