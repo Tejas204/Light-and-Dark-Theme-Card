9e28363497dc6d2c525d21a5503fd28f8d22865a
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2024-03-29 22:32:25 +0530

    Styled Card

--- src/component/Card.js
+++ src/component/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = () => {
   return (
     <div className='flex h-screen justify-center items-center'>
-        <div className='flex flex-col gap-y-8 items-center p-5 md:p-10 w-3/4 lg:w-2/4 xl:w-1/4 border-2 rounded-xl'>
+        <div className='flex flex-col gap-y-8 items-center p-5 md:p-10 w-3/4 lg:w-2/4 xl:w-1/4 border-2 rounded-xl shadow-gray-400 shadow-md'>
             <p className='text-md md:text-xl lg:text-2xl font-bold'>
                 Card
             </p>
@@ -12,7 +12,7 @@ const Card = () => {
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             </p>
-            <button className='text-sm md:text-md lg:text-lg border-2 rounded-lg p-3'>
+            <button className={`text-sm md:text-md lg:text-lg border-2 rounded-lg p-3 font-bold text-white bg-purple-500`}>
                 Submit
             </button>
         </div>
